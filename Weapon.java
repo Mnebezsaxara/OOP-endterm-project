e7af560f0f6de0bb3565075aaf73cd6ee876d2c2
Mansur <155591937+Mnebezsaxara@users.noreply.github.com>
2024-02-26 19:55:52 +0600

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,17 @@
+
+public class Weapon extends Item {
+    private int damage;
+
+    public Weapon(String name, String description, int damage) {
+        super(name, description);
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void attack() {
+        System.out.println("Attacking with " + getName() + "!");
+    }
+}
